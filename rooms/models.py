6588855399d5438c975063cabd8578b580e6f5da
ZzicC12 <pyw251@gmail.com>
2021-02-17 17:06:33 +0900

    4.3

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
 
 
 class Room(core_models.TimeStampedModel):
@@ -21,4 +38,8 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
